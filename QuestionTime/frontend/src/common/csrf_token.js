c99a1033947d7552ea45fda240138483012d425c
haruyasu <harukun2002@gmail.com>
2020-02-07 21:31:18 +0900

    fix warning

--- QuestionTime/frontend/src/common/csrf_token.js
+++ QuestionTime/frontend/src/common/csrf_token.js
@@ -1,11 +1,11 @@
 function getCookie(name) {
   var cookieValue = null;
-  if (document.cookie && document.cookie !== '') {
-    var cookies = document.cookie.split(';');
+  if (document.cookie && document.cookie !== "") {
+    var cookies = document.cookie.split(";");
     for (var i = 0; i < cookies.length; i++) {
       var cookie = cookies[i].trim();
       // Does this cookie string begin with the name we want?
-      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+      if (cookie.substring(0, name.length + 1) === name + "=") {
         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
         break;
       }
@@ -13,6 +13,6 @@ function getCookie(name) {
   }
   return cookieValue;
 }
-var CSRF_TOKEN = getCookie('csrftoken');
+var CSRF_TOKEN = getCookie("csrftoken");
 
 export { CSRF_TOKEN };
--- QuestionTime/frontend/src/views/Home.vue
+++ QuestionTime/frontend/src/views/Home.vue
@@ -1,46 +1,44 @@
 <template>
   <div class="home">
     <div class="container">
-      <div v-for="question in questions"
-            :key="question.pk">
-        <p class="mb-0">Posted by:
-          <span class="author-name">{{ question.author }}</span>
+      <div v-for="question in questions" :key="question.pk">
+        <p class="mb-0">
+          Posted by:<span class="author-name">{{ question.author }}</span>
         </p>
         <h2>{{ question.content }}</h2>
         <p>Answers: {{ question.answers_count }}</p>
-        <hr>
+        <hr />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { apiService } from "../common/api.service"
+import { apiService } from "../common/api.service";
 export default {
   name: "home",
   data() {
     return {
       questions: []
-    }
+    };
   },
   methods: {
     getQuestions() {
       let endpoint = "api/questions/";
-      apiService(endpoint)
-          .then(data => {
-            this.questions.push(...data.results)
-          })
+      apiService(endpoint).then(data => {
+        this.questions.push(...data.results);
+      });
     }
   },
   created() {
-    this.getQuestions()
-  },
+    this.getQuestions();
+  }
 };
 </script>
 
 <style>
-  .author-name {
-    font-weight: bold;
-    color: #DC3545;
-  }
+.author-name {
+  font-weight: bold;
+  color: #dc3545;
+}
 </style>
