51315edbc663bc507d093389a6d9138f945ae69c
haruyasu <harukun2002@gmail.com>
2020-02-08 13:31:36 +0900

    added next button

--- QuestionTime/frontend/src/views/Home.vue
+++ QuestionTime/frontend/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <div class="container">
+    <div class="container mt-2">
       <div v-for="question in questions" :key="question.pk">
         <p class="mb-0">
           Posted by:<span class="question-author">{{ question.author }}</span>
@@ -16,6 +16,16 @@
         <p>Answers: {{ question.answers_count }}</p>
         <hr />
       </div>
+      <div class="my-4">
+        <p v-show="loadingQuestions">...loading...</p>
+        <button
+          v-show="next"
+          @click="getQuestions"
+          class="btn btn-sm btn-outline-success"
+        >
+          Lead More
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -26,14 +36,26 @@ export default {
   name: "home",
   data() {
     return {
-      questions: []
+      questions: [],
+      next: null,
+      loadingQuestions: false
     };
   },
   methods: {
     getQuestions() {
       let endpoint = "/api/questions/";
+      if (this.next) {
+        endpoint = this.next;
+      }
+      this.loadingQuestions = true;
       apiService(endpoint).then(data => {
         this.questions.push(...data.results);
+        this.loadingQuestions = false;
+        if (data.next) {
+          this.next = data.next;
+        } else {
+          this.next = null;
+        }
       });
     }
   },
