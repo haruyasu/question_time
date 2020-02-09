44c70b52c651e034fe367b2774631bdda6833e8a
haruyasu <harukun2002@gmail.com>
2020-02-09 17:48:43 +0900

    heroku

--- QuestionTime/settings.py
+++ QuestionTime/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -169,3 +170,5 @@ WEBPACK_LOADER = {
         'STATS_FILE': os.path.join(BASE_DIR, 'frontend', 'webpack-stats.json')
     }
 }
+
+django_heroku.settings(locals())
